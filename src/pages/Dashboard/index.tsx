1d44702c5672177832b1dde09806f0dbbf8629b1
nelson kenzo tamashiro <nelsonkenzotamashiro@gmail.com>
2020-05-27 21:48:48 -0300

    Implemented interface

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, {
+    useCallback,
+    useState,
+} from 'react';
+
+import DayPicker, {
+    DayModifiers,
+} from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 import {
     Container,
     Header,
     HeaderContent,
     Profile,
+    Content,
+    Schedule,
+    Calendar,
+    NextAppointment,
+    Section,
+    Appointment,
 } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { randomIcon } from '../../services/avatar';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 import { useAuth } from '../../context/Auth';
 
 const Dashboard: React.FC = () => {
 
-    const { user, signOut } =  useAuth();
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const { user, signOut } = useAuth();
+
+    const handleDateSelected = useCallback((day: Date, modifiers: DayModifiers) => {
+        if (modifiers.available) {
+
+            setSelectedDate(day);
+        }
+    }, []);
     return (
         <Container>
             <Header>
@@ -38,6 +61,100 @@ const Dashboard: React.FC = () => {
                     </button>
                 </HeaderContent>
             </Header>
+
+            <Content>
+                <Schedule>
+                    <h1>Horários Agendados</h1>
+                    <p>
+                        <span>{'Hoje'}</span>
+                        <span>{'Dia 6'}</span>
+                        <span>{'Segunda-feira'}</span>
+                    </p>
+
+                    <NextAppointment>
+                        <strong>{'Atendimento a seguir'}</strong>
+                        <div>
+                            <img src={randomIcon()} alt="user icon" />
+                            <strong>{'Client name'}</strong>
+
+                            <span>
+                                <FiClock />
+                                {'08:00'}
+                            </span>
+                        </div>
+
+                    </NextAppointment>
+
+                    <Section>
+                        <strong>Manhã</strong>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                {'08:00'}
+                            </span>
+
+                            <div>
+                                <img src={randomIcon()} alt="user icon" />
+                                <strong>{'Client name'}</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
+
+                    <Section>
+                        <strong>Tarde</strong>
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                {'08:00'}
+                            </span>
+
+                            <div>
+                                <img src={randomIcon()} alt="user icon" />
+                                <strong>{'Client name'}</strong>
+                            </div>
+                        </Appointment>
+
+                        <Appointment>
+                            <span>
+                                <FiClock />
+                                {'08:00'}
+                            </span>
+
+                            <div>
+                                <img src={randomIcon()} alt="user icon" />
+                                <strong>{'Client name'}</strong>
+                            </div>
+                        </Appointment>
+                    </Section>
+
+                </Schedule>
+                <Calendar>
+                    <DayPicker
+                        fromMonth={new Date()}
+                        selectedDays={selectedDate}
+                        disabledDays={[{ daysOfWeek: [0] }]}
+                        modifiers={{
+                            available: { daysOfWeek: [0, 1, 2, 3, 4, 5, 6] }
+                        }}
+                        onDayClick={handleDateSelected}
+                        weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+                        months={[
+                            'Janeiro',
+                            'Fevereiro',
+                            'Março',
+                            'Abril',
+                            'Maio',
+                            'Junho',
+                            'Julho',
+                            'Agosto',
+                            'Setembro',
+                            'Outubro',
+                            'Novembro',
+                            'Dezembro',
+                        ]}
+                    />
+                </Calendar>
+            </Content>
         </Container>
     );
 }
